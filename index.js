42a269b27682610a5359bee4235166967028aa7c
Samuel Mandel <slman.760@gmail.com>
2022-05-02 21:45:48 -0700

    add routes for the selection of a list with inquirer

--- index.js
+++ index.js
@@ -30,5 +30,52 @@ function menu(){
                 ]
             }
         ])
+        .then((res) =>{
+            if(res.start === 'View All Employees'){
+                viewEmployees();
+            } else if(res.start === ''){
+                addEmployee();
+            } else if(res.start === ''){
+                updateEmployee();
+            } else if(res.start === ''){
+                viewRoles();
+            } else if(res.start === ''){
+                addRole();
+            } else if(res.start === ''){
+                viewDepartments();
+            } else if(res.start === ''){
+                addDepartment();
+            } else if(res.start === ''){
+                
+            } 
+        })
+}
+//view Departments 
+function viewDepartments() {
+
+}
+//add to a department
+function addDepartment() {
+
+}
+//view roles
+function viewRoles(){
+
+}
+//add a role
+function addRole(){
+
+}
+//view employees
+function viewEmployees(){
+
+}
+//add employee
+function addEmployee(){
+
+}
+//update employee
+function updateEmployee(){
+
 }
 menu();
